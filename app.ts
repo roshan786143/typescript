6a9ae8081976039116ab82a25d45f874975363bd
roshan786143 <roshanbabu.kng@gmail.com>
2023-01-14 17:04:29 +0530

    rest routes

--- app.ts
+++ app.ts
@@ -2,8 +2,6 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
-const numResults: number[]  = [];
-const textResults: string[] = [];
 
 function add(num1: number | string, num2: number | string) {
   if (typeof num1 === 'number' && typeof num2 === 'number') {
@@ -14,9 +12,6 @@ function add(num1: number | string, num2: number | string) {
   return +num1 + +num2;
 }
 
-function printResult(resultObj: { val: number; timestamp: Date }) {
-  console.log(resultObj.val);
-}
 
 buttonElement.addEventListener('click', () => {
   const num1 = num1Element.value;
